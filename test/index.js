22acf997e317b561e3c6f71e2aa0949ce6c7a475
Caio Gondim <me@caiogondim.com>
2016-08-14 18:07:09 +0200

    100% test coverage

--- test/index.js
+++ test/index.js
@@ -14,6 +14,25 @@ tap.test('should run callback if main thread is obstructed', (test) => {
 
   console.log(`:`)
   debugMsleep.log('blocking thread')
-  msleep(500)
+  msleep(110)
   debugMsleep.log('releasing thread')
+})
+
+tap.test('should not run callback if main thread is obstructed but threshold is higher', (test) => {
+  let didCallbackRun = false
+
+  const obstructedTimer = obstructed((time) => {
+    let didCallbackRun = true
+  }, {threshold: 100})
+
+  console.log(`:`)
+  debugMsleep.log('blocking thread')
+  msleep(50)
+  debugMsleep.log('releasing thread')
+
+  setTimeout(() => {
+    clearInterval(obstructedTimer)
+    test.ok(!didCallbackRun)
+    test.end()
+  }, 100)
 })
