31a5cf6a31e8818547a114f6fd63a24e8a9d8767
Caio Gondim <me@caiogondim.com>
2016-08-14 17:49:15 +0200

    Add test suite

--- test/index.js
+++ test/index.js
@@ -1,10 +1,19 @@
+const tap = require('tap')
+const logdown = require('logdown')
 const msleep = require('msleep')
 const obstructed = require('../src/index')
 
-obstructed((t) => {
-  console.log(`main thread was obstructed for about ${t}ms`)
-})
+const debugMsleep = logdown({prefix: 'msleep'})
 
-console.log(Date.now())
-msleep(1000)
-console.log(Date.now())
+tap.test('should run callback if main thread is obstructed', (test) => {
+  const obstructedTimer = obstructed((time) => {
+    clearInterval(obstructedTimer)
+
+    test.end()
+  })
+
+  console.log(`:`)
+  debugMsleep.log('blocking thread')
+  msleep(500)
+  debugMsleep.log('releasing thread')
+})
