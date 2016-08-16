a0dce0bdb875a2d4937eeee52dd97f29c0801aba
Caio Gondim <me@caiogondim.com>
2016-08-16 18:40:06 +0200

    Add build task

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  output: {
+    library: 'obstructed',
+    libraryTarget: 'umd',
+    path: './dist',
+    filename: 'obstructed.js'
+  },
+  entry: {
+    library: './src/index.js'
+  }
+}
